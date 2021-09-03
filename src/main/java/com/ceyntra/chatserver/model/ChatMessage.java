ff831f65df6179d40d0b019c1b2294ee891f18ef
shashiradw <shashiradw7@gmail.com>
2021-09-03 12:41:38 +0530

    ChatMessage model for private chat

--- src/main/java/com/ceyntra/chatserver/model/ChatMessage.java
+++ src/main/java/com/ceyntra/chatserver/model/ChatMessage.java
@@ -1,2 +1,36 @@
-package com.ceyntra.chatserver.model;public class ChatMessage {
+package com.ceyntra.chatserver.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "chatMessage")
+public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int senderId; //
+    private int recipientId; //
+    private String senderName;
+    private String recipientName;
+    private String content; //
+    private Date timestamp; //
+
+    @ManyToOne
+    @JsonIgnore
+    private ChatRoom chatRoom;
+
+
+
 }
