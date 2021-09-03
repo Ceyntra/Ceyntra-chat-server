d38dd246f6bfc0e9fd6fd2b42a0e891556581788
shashiradw <shashiradw7@gmail.com>
2021-09-03 12:44:22 +0530

    Private chat repository created

--- src/main/java/com/ceyntra/chatserver/repository/PrivateChatMessageRepository.java
+++ src/main/java/com/ceyntra/chatserver/repository/PrivateChatMessageRepository.java
@@ -1,2 +1,9 @@
-package com.ceyntra.chatserver.repository;public interface PrivateChatMessageRepository {
+package com.ceyntra.chatserver.repository;
+
+import com.ceyntra.chatserver.model.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PrivateChatMessageRepository extends JpaRepository<ChatMessage,Integer> {
 }
